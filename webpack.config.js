75444d3026aa9da2c5e3f352ff53c048df0e9133
Kashyap Mukkamala <kmukkamala@egen.solutions>
2018-08-02 04:27:13 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    mode: "development",
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    context: path.resolve('js'),
+    entry: {
+        about: './about_page.js',
+        home: './home_page.js',
+        contact: './contact_page.js'
+    },
+    output: {
+        path: path.resolve('build/js/'),
+        publicPath: '/public/assets/js/',
+        filename: "[name].js"
+    },
+    devServer: {
+        contentBase: 'public'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.es6']
+    },
+    watch: true
+}
